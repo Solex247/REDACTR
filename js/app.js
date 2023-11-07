1ab47f0c8f002fc98313d489a7ca4f2a45bc8179
Adams Solomon <solex247@gmail.com>
2023-11-07 23:50:28 +0100

    ╬ô├╢┬╝Γö£ΓòùDisplay stats like how many words were scanned, how many matched the words to be scrambled, how many characters were scrambled in total, how long it took (in seconds) to complete the scrambling task etc.

--- js/app.js
+++ js/app.js
@@ -3,12 +3,31 @@ document.getElementById("redact-button").addEventListener("click", function () {
     const redactWords = document.getElementById("redact-words").value.split(" ");
     const replacementText = document.getElementById("replacement-text").value;
 
+    const startTime = Date.now(); // Start time
+
     let redactedText = originalText;
+
+    let wordsScanned = 0;
+    let wordsMatched = 0;
+    let charactersScrambled = 0;
 
     redactWords.forEach((word) => {
         const regex = new RegExp("\\b" + word + "\\b", "gi");
-        redactedText = redactedText.replace(regex, replacementText);
+        const matches = redactedText.match(regex);
+
+        if (matches) {
+            wordsMatched += matches.length;
+            charactersScrambled += word.length * matches.length;
+            redactedText = redactedText.replace(regex, replacementText);
+        }
     });
 
+    const endTime = Date.now(); // End time
+    const timeTaken = ((endTime - startTime) / 1000).toFixed(2); // Calculate time taken in seconds
+
     document.getElementById("redacted-output").textContent = redactedText;
+    document.getElementById("words-scanned").textContent = redactWords.length;
+    document.getElementById("words-matched").textContent = wordsMatched;
+    document.getElementById("characters-scrambled").textContent = charactersScrambled;
+    document.getElementById("time-taken").textContent = timeTaken;
 });
